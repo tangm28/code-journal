577f249ec74d4f4773889d88c198715d3d168287
Michael Tang <tngmichael28@gmail.com>
2021-12-21 15:38:27 -0800

    Removed event listener for load and set data to previousEntries.

--- js/data.js
+++ js/data.js
@@ -8,19 +8,14 @@ var data = {
   nextEntryId: 1
 };
 
-function loadData(event) {
-  previousEntries = localStorage.getItem('entries-local-storage');
-  if (previousEntries !== null) {
-    data.entries = JSON.parse(previousEntries).entries;
-    data.nextEntryId = JSON.parse(previousEntries).nextEntryId++;
-  }
-}
-
 function saveDataB4Unload(event) {
   if (data.entries.length > 0) {
     localStorage.setItem('entries-local-storage', JSON.stringify(data));
   }
 }
 
-window.addEventListener('load', loadData);
+previousEntries = localStorage.getItem('entries-local-storage');
+if (previousEntries !== null) {
+  data = JSON.parse(previousEntries);
+}
 window.addEventListener('beforeunload', saveDataB4Unload);
