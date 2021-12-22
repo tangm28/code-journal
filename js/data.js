3b753653d3e41779761961830986adc33af345e0
Michael Tang <tngmichael28@gmail.com>
2021-12-22 13:36:15 -0800

    Entries can be created from data and removed dummy HTML entries.

--- js/data.js
+++ js/data.js
@@ -1,4 +1,5 @@
 /* exported data */
+var previousEntries;
 
 var data = {
   view: 'entry-form',
@@ -6,3 +7,24 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+function loadData(event) {
+  previousEntries = localStorage.getItem('entries-local-storage');
+  if (previousEntries !== null) {
+    data.entries = JSON.parse(previousEntries).entries;
+    data.nextEntryId = JSON.parse(previousEntries).nextEntryId++;
+  }
+}
+
+function saveDataB4Unload(event) {
+  if (data.entries.length > 0) {
+    localStorage.setItem('entries-local-storage', JSON.stringify(data));
+  }
+}
+
+window.addEventListener('load', loadData);
+previousEntries = localStorage.getItem('entries-local-storage');
+if (previousEntries !== null) {
+  data = JSON.parse(previousEntries);
+}
+window.addEventListener('beforeunload', saveDataB4Unload);
--- js/main.js
+++ js/main.js
@@ -5,65 +5,56 @@ var $photoUrl = document.querySelector('.photo-url');
 var $notes = document.querySelector('.notes');
 var $form = document.querySelector('form');
 var $listOfEntries = document.querySelector('.list-of-entries');
+var $noEntries = document.querySelector('.no-entries');
 function uploadPhoto(event) {
   $photo.setAttribute('src', $photoUrl.value);
 }
 
 function saveEntry(event) {
   event.preventDefault();
-  var previousEntries = localStorage.getItem('entries-local-storage');
   var entry = {
     title: $title.value,
     photoUrl: $photoUrl.value,
     notes: $notes.value,
     nextEntryId: data.nextEntryId
   };
-  if (previousEntries !== null) {
-    data.entries.concat(JSON.parse(previousEntries).entries);
-  }
   data.nextEntryId++;
   data.entries.unshift(entry);
-  localStorage.setItem('entries-local-storage', JSON.stringify(data));
   $photo.setAttribute('src', 'images/placeholder-image-square.jpg');
-  $title.value = '';
-  $photoUrl.value = '';
-  $notes.value = '';
+  $form.reset();
 }
 
-/* <li class="row">
-  <img src="images/placeholder-image-square.jpg" alt="No image" class="photo column-half">
-  <div class ="column-half">
-  <h2>Title</h2>
-  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.Perspiciatis repudiandae voluptate recusandae eum ipsa id.Perferendis minima commodi nihil nulla, modi quod veritatis.Modi, dolorem!</p>
-  </div>
-</li> */
-
-function renderEntries(event) {
+function renderEntry(entry) {
   var liRow = document.createElement('li');
   liRow.setAttribute('class', 'row');
   $listOfEntries.appendChild(liRow);
   var imgPhotoColumnHalf = document.createElement('img');
-  imgPhotoColumnHalf.setAttribute('src', 'images/placeholder-image-square.jpg');
-  imgPhotoColumnHalf.setAttribute('alt', 'No image');
+  imgPhotoColumnHalf.setAttribute('src', entry.photoUrl);
+  imgPhotoColumnHalf.setAttribute('alt', 'Photo of ' + entry.title);
   imgPhotoColumnHalf.setAttribute('class', 'photo column-half');
   liRow.appendChild(imgPhotoColumnHalf);
   var divColumnHalf = document.createElement('div');
   divColumnHalf.setAttribute('class', 'column-half');
   liRow.appendChild(divColumnHalf);
   var h2Title = document.createElement('h2');
-  h2Title.textContent = 'Title';
+  h2Title.textContent = entry.title;
   var pEntries = document.createElement('p');
-  pEntries.textContent = 'Lorem50';
+  pEntries.textContent = entry.notes;
   divColumnHalf.appendChild(h2Title);
   divColumnHalf.appendChild(pEntries);
 }
 
-renderEntries(event);
+function createEntries(event) {
+  for (var i = 0; i < data.entries.length; i++) {
+    renderEntry(data.entries[i]);
+  }
+}
 
 $photoUrl.addEventListener('input', uploadPhoto);
 $form.addEventListener('submit', saveEntry);
-window.addEventListener('beforeunload', function (event) {
-  var previousEntries = localStorage.getItem('entries-local-storage');
-  localStorage.setItem('entries-local-storage', previousEntries);
-});
+if (data.entries < 1) {
+  $noEntries.setAttribute('class', 'row no-entries justify-center');
+} else {
+  window.addEventListener('DOMContentLoaded', createEntries(event));
+}
 /* exported data */
